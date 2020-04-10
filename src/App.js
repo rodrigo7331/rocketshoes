be1a147c7a9269b2688e58c960da5c2f121176ec
rodrigo7331 <rodrigo7331@hotmail.com>
2020-04-10 11:49:27 -0300

    Design da home e store terminados.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
 
 function App() {
-  return <h1>Hello World</h1>;
-}
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
 
+      <GlobalStyles />
+    </BrowserRouter>
+  );
+}
 export default App;
